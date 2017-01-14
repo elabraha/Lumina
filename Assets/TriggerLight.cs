74144e16de3c0742091a91d70edef5bc46ff4f1c
Yubing Xie <xieyubin@umich.edu>
2017-01-14 02:56:59 -0500

    second

---
+++ Assets/TriggerLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLight : MonoBehaviour {
+	public Light ConnectLight;
+
+	void start(){
+		ConnectLight.enabled = false;
+	}
+	void OnTriggerEnter(){
+		ConnectLight.enabled = true;
+	}
+
+	void OnTriggerExit(){
+		ConnectLight.enabled = false;
+	}
+
+
+}
